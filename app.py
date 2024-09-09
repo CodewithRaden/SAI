2a97a3505aa11c1ec4d551c45861f60d99b0dedf
CodewithRaden <fadilazhar2003@gmail.com>
2024-09-09 19:12:45 +0700

    Vercel-Deploy

--- app.py
+++ app.py
@@ -8,12 +8,17 @@ from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 from sklearn.neighbors import KDTree
 import sqlite3
+import requests
+import pandas as pd
+import numpy as np
+import psycopg2
 
 app = Flask(__name__)
 app.secret_key = 'matherfather'
 
 
 import sqlite3
+
 
 def init_db():
     conn = sqlite3.connect('attendance.db')
@@ -57,56 +62,66 @@ def attendance():
         pin = request.form.get('pin')
         action = request.form.get('action')
 
-        conn = sqlite3.connect('attendance.db')
+        # Establish a connection to the PostgreSQL database
+        conn = psycopg2.connect(
+            database="verceldb", 
+            user='default', 
+            password='lfQrZe6KxYi5', 
+            host='ep-billowing-darkness-a1nux2qh-pooler.ap-southeast-1.aws.neon.tech', 
+            port='5432'
+        )
         cursor = conn.cursor()
 
-        cursor.execute('SELECT * FROM user WHERE pin = ?', (pin,))
-        user = cursor.fetchone()
+        # Debug: Print the PIN that is being looked up
+        print(f"Searching for PIN: {pin}")
 
-        if not user:
+        # Find the Pegawai associated with the provided PIN
+        cursor.execute('SELECT id, "namaPegawai" FROM public."Pegawai" WHERE pin = %s', (pin,))
+        pegawai = cursor.fetchone()
+
+        # Debug: Print what was fetched
+        print(f"Fetched Pegawai: {pegawai}")
+
+        if not pegawai:
             flash('Invalid PIN. Please try again.', 'error')
             conn.close()
             return redirect(url_for('attendance'))
 
-        user_id = user[0]  
-        name = user[1]  
+        pegawai_id = pegawai[0]  
+        name = pegawai[1]  
     
         today = datetime.now().strftime('%Y-%m-%d')
-        current_time = datetime.now().strftime('%H:%M:%S')
 
-        cursor.execute('SELECT * FROM attendance WHERE user_id = ? AND tanggal = ?', (user_id, today))
+        # Check if there's an attendance record for today
+        cursor.execute('SELECT * FROM public."Absensi" WHERE "pegawaiId" = %s AND DATE("waktuMasuk") = %s', (pegawai_id, today))
         attendance_record = cursor.fetchone()
 
+        # Check-in Action
         if action == 'check_in':
-            if attendance_record and attendance_record[4]:
-                flash(f'{name}  already checked in today.', 'error')
+            if attendance_record:
+                flash(f'{name} already checked in today.', 'error')
             else:
-                cursor.execute('''
-                    INSERT INTO attendance (user_id, nama, tanggal, waktu_masuk, status)
-                    VALUES (?, ?, ?, ?, ?)
-                ''', (user_id, name, today, current_time, 'Hadir'))
                 conn.commit()
-                flash(f'Check-in successful for {name} at {current_time}.', 'success')
+                flash(f'Check-in successful for {name}.', 'success')
+                send_attendance_data_to_server(pegawai_id, 'check_in', today)
 
+        # Check-out Action
         elif action == 'check_out':
-            if not attendance_record or not attendance_record[4]: 
-                flash(f'{name} have not checked in today.', 'error')
-            elif attendance_record[5]: 
-                flash(f'{name} have already checked out today.', 'error')
+            if not attendance_record:
+                flash(f'{name} has not checked in today.', 'error')
+            elif attendance_record[2]:  # Assuming "waktuKeluar" is the third column in the result
+                flash(f'{name} has already checked out today.', 'error')
             else:
-                cursor.execute('''
-                    UPDATE attendance
-                    SET waktu_keluar = ?, status = ?
-                    WHERE user_id = ? AND tanggal = ?
-                ''', (current_time, 'Hadir', user_id, today))
                 conn.commit()
-                flash(f'Check-out successful for {name} at {current_time}.', 'success')
+                flash(f'Check-out successful for {name}.', 'success')
+                send_attendance_data_to_server(pegawai_id, 'check_out', today)
 
+
+        # Close the connection
         conn.close()
         return redirect(url_for('attendance'))
 
     return render_template('pin_attendance.html')
-
 
 
 @app.route('/capture', methods=['POST'])
@@ -129,7 +144,7 @@ def capture():
     recognized_names = []
     message = ""
 
-    name = None 
+    name = None
 
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         match_index = find_matches(face_encoding)
@@ -143,48 +158,58 @@ def capture():
         draw.rectangle(((left, bottom), (left + text_width, bottom + text_height)), fill=(0, 0, 255), outline=(0, 0, 255))
         draw.text((left, bottom), name, fill=(255, 255, 255, 255), font=font)
 
-    if name and name != "Unknown": 
-        conn = sqlite3.connect('attendance.db')
+    if name and name != "Unknown":
+        # Connect to PostgreSQL
+        conn = psycopg2.connect(
+            database="verceldb", 
+            user='default', 
+            password='lfQrZe6KxYi5', 
+            host='ep-billowing-darkness-a1nux2qh-pooler.ap-southeast-1.aws.neon.tech', 
+            port='5432'
+        )
         cursor = conn.cursor()
 
-        cursor.execute('SELECT id FROM user WHERE name = ?', (name,))
-        user_id = cursor.fetchone()
+        # Find Pegawai by name
+        cursor.execute('SELECT id FROM public."Pegawai" WHERE "namaPegawai" = %s', (name,))
+        pegawai = cursor.fetchone()
 
-        if user_id:
-            user_id = user_id[0]
+        if pegawai:
+            user_id = pegawai[0]
             now = datetime.now()
             today = now.strftime('%Y-%m-%d')
             current_time = now.strftime('%H:%M:%S')
             saved_image_path = save_image(pil_image, name, attendance_type)
 
+            # Check if there's an attendance record for today
             cursor.execute('''
-                SELECT * FROM attendance WHERE user_id = ? AND tanggal = ?
+                SELECT * FROM public."Absensi" 
+                WHERE "pegawaiId" = %s AND DATE("waktuMasuk") = %s
             ''', (user_id, today))
             record = cursor.fetchone()
 
             if attendance_type == 'checkin':
                 if record:
-                    message = f'{name} sudah melakukan check-in hari ini.'
+                    message = f'{name} already checked in today.'
                 else:
-                    keterlambatan = "Ya" if now.time() > datetime.strptime("09:15:00", '%H:%M:%S').time() else "Tidak"
-                    cursor.execute('''
-                        INSERT INTO attendance (user_id, nama, tanggal, waktu_masuk, keterlambatan, status, checkin_image)
-                        VALUES (?, ?, ?, ?, ?, ?, ?)
-                    ''', (user_id, name, today, current_time, keterlambatan, 'Hadir', saved_image_path))
-                    message = f'{name} berhasil check-in.'
+                    message = f'{name} successfully checked in.'
+                    send_attendance_data_to_server(user_id, 'check_in', current_time)
 
             elif attendance_type == 'checkout':
-                if not record or not record[4]:
-                    message = f'{name} belum melakukan check-in hari ini.'
-                elif record[5]: 
-                    message = f'{name} sudah melakukan check-out hari ini.'
+                if not record:
+                    message = f'{name} has not checked in today.'
+                elif record[2]:  # Assuming "waktuKeluar" is the third column in the result
+                    message = f'{name} already checked out today.'
                 else:
                     cursor.execute('''
-                        UPDATE attendance SET waktu_keluar = ?, status = ?, checkout_image = ? WHERE user_id = ? AND tanggal = ?
-                    ''', (current_time, 'Hadir', saved_image_path, user_id, today))
-                    message = f'{name} berhasil check-out.'
+                        UPDATE public."Absensi" 
+                        SET "waktuKeluar" = %s 
+                        WHERE "pegawaiId" = %s AND DATE("waktuMasuk") = %s
+                    ''', (current_time, user_id, today))
+                    message = f'{name} successfully checked out.'
+                    send_attendance_data_to_server(user_id, 'check_out', current_time)
 
             conn.commit()
+
         conn.close()
 
     del draw
@@ -192,6 +217,7 @@ def capture():
     pil_image.save(annotated_image_path)
 
     return jsonify({'message': message, 'names': recognized_names, 'image_path': annotated_image_path})
+
 
 
 def resize_image(image, target_width=500):
@@ -269,18 +295,38 @@ def register_combined():
     if not name or not pin or not files:
         return jsonify({'error': 'Name, PIN, and photos are required'}), 400
 
-    conn = sqlite3.connect('attendance.db')
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(
+        database="verceldb", 
+        user='default', 
+        password='lfQrZe6KxYi5', 
+        host='ep-billowing-darkness-a1nux2qh-pooler.ap-southeast-1.aws.neon.tech', 
+        port='5432'
+    )
     cursor = conn.cursor()
 
-    cursor.execute('SELECT * FROM user WHERE pin = ?', (pin,))
+    # Check if the PIN is already in use
+    cursor.execute('SELECT * FROM public."Pegawai" WHERE pin = %s', (pin,))
     existing_user = cursor.fetchone()
 
     if existing_user:
+        conn.close()
         return jsonify({'error': 'PIN already in use. Please choose another PIN.'}), 400
 
-
-    cursor.execute('INSERT INTO user (name, pin) VALUES (?, ?)', (name, pin))
+    # Update the Pegawai record with the provided PIN
+    cursor.execute('''
+        UPDATE public."Pegawai" 
+        SET pin = %s 
+        WHERE "namaPegawai" = %s
+        RETURNING id
+    ''', (pin, name))
     conn.commit()
+
+    updated_pegawai_id = cursor.fetchone()
+
+    if not updated_pegawai_id:
+        conn.close()
+        return jsonify({'error': 'No Pegawai found with that name. Please check the name and try again.'}), 400
 
     person_dir = os.path.join('faces', name)
     if not os.path.exists(person_dir):
@@ -293,11 +339,62 @@ def register_combined():
     load_known_faces('faces')
     conn.close()
 
-    return jsonify({'message': f'{name} has been registered successfully.'}), 200
+    return jsonify({'message': f'{name} has been registered successfully with PIN {pin}.'}), 200
+
 
 @app.route('/register_combined_view')
 def register_combined_view():
     return render_template('register_combined.html')
+
+
+# Add New api for sai?
+
+def send_attendance_data_to_server(user_id, action, time):
+    url = f"http://sai-web-alpha.vercel.app/api/addAbsensi/{user_id}"
+    headers = {
+        "x-vercel-protection-bypass": "fx7j8AGuf6OHN4FlXqWJjcrLhEYxIArj"
+    }
+    
+    data = {
+        "user_id": user_id,
+        "action": action,
+        "time": time
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+    
+    if response.status_code == 200:
+        print(f"Successfully sent {action} data to server for user_id {user_id}.")
+    else:
+        print(f"Failed to send data to server. Status Code: {response.status_code}. Response: {response.text}")
+        
+
+@app.route('/get_pg_names', methods=['GET'])
+def get_pg_names():
+    try:
+        # Establish a connection to the PostgreSQL database
+        conn = psycopg2.connect(
+            database="verceldb", 
+            user='default', 
+            password='lfQrZe6KxYi5', 
+            host='ep-billowing-darkness-a1nux2qh-pooler.ap-southeast-1.aws.neon.tech', 
+            port='5432'
+        )
+        cursor = conn.cursor()
+
+        # Execute the query to fetch names of Pegawai
+        cursor.execute('SELECT "namaPegawai" FROM public."Pegawai"')
+        names = cursor.fetchall()
+
+        # Close the connection
+        conn.close()
+
+        # Return the names as a JSON array
+        return jsonify([name[0] for name in names])
+
+    except Exception as e:
+        # Handle exceptions, such as database connection errors
+        return jsonify({'error': str(e)}), 500
 
 
 if __name__ == '__main__':
